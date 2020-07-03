090a80adf613405ab9f19aa31dc20a86712673ff
Roidy <roidy.1207@gmail.com>
2020-07-02 22:47:50 -0500

    ver2

--- src/Server.js
+++ src/Server.js
@@ -10,6 +10,12 @@ app.use(express.json());
 //Routes
 app.use(require('./routes/datos'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Starting the server
 app.listen(app.get('port'), () => {
